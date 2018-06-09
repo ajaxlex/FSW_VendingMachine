5effbe017899e39f82ce34b1c57f0f3d0050529f
Ajaxlex <ajaxlex@gmail.com>
2018-06-09 12:16:34 -0400

    reorganized with less serial

--- MotorControllerMain/MotorControllerMain.ino
+++ MotorControllerMain/MotorControllerMain.ino
@@ -1,5 +1,5 @@
-#define COIN_IN 13
-#define COIN_OUT 12
+#define COIN_IN 0
+#define COIN_OUT 1
 
 #include <SoftwareSerial.h>
 SoftwareSerial coinSerial = SoftwareSerial(COIN_IN,COIN_OUT);
@@ -9,35 +9,77 @@ SoftwareSerial coinSerial = SoftwareSerial(COIN_IN,COIN_OUT);
 
 #define VENDING 9
 
+#define ledPin 13
+
 #define EOL 255
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(COIN_IN, INPUT);
-  pinMode(COIN_OUT, OUTPUT);
 
-  Serial.begin(9600);
-  Serial.println("motor spirals ready");
-  coinSerial.begin(4800);
+  pinMode(ledPin, OUTPUT);
+  
+  // put your setup code here, to run once:
+  //pinMode(COIN_IN, INPUT);
+  //pinMode(COIN_OUT, OUTPUT);
+
+  Serial.begin(4800);
+  //Serial.println("motor spirals ready");
+  //coinSerial.begin(4800);
 }
 
 void loop() {
-  coinSerial.listen();
+  //coinSerial.listen();
+  //coinSerial.write(HANDSHAKE);
+  responseDIRECT();
+}
+
+void responseDIRECT(){
+  if (Serial.available()) {
+    int c = Serial.read();
+    //debugChar(c);
+    if ( c != EOL ) {
+      //if ( c == TEST_VAL ) {
+        slowBlink();
+      //} else if ( c == 1 ) {
+      //  coinSerial.write(VENDING);
+      //  Serial.print(c);
+      //}
+    }
+  }    
+}
+
+void ssResponse(){
   if (coinSerial.available()) {
     int c = coinSerial.read();
     debugChar(c);
     if ( c != EOL ) {
-      if ( c == TEST_VAL ) {
+      //if ( c == TEST_VAL ) {
         coinSerial.write(HANDSHAKE);
-      } else if ( c == 1 ) {
-        coinSerial.write(VENDING);
-        Serial.print(c);
-      }
+      //} else if ( c == 1 ) {
+      //  coinSerial.write(VENDING);
+      //  Serial.print(c);
+      //}
     }
-  }
+  }  
 }
-
 
 void debugChar( char c ){
   Serial.print(c);
+}
+
+void fastBlink(){
+  blink( 9, 300 );
+}
+
+void slowBlink(){
+  blink(3, 900);
+  Serial.write(HANDSHAKE);
+}
+
+void blink( int count, int delayTime ){
+  for ( int i=0; i<count; i++) {
+    digitalWrite(ledPin, HIGH);   // sets the LED on
+    delay(delayTime);                  // waits for a third second
+    digitalWrite(ledPin, LOW);    // sets the LED off
+    delay(delayTime);
+  }
 }
--- VendingMachineMain/VendingMachineMain.ino
+++ VendingMachineMain/VendingMachineMain.ino
@@ -14,11 +14,12 @@
 #include <Bounce2.h>
 
 #define COIN_IN 11
-#define COIN_NULL 10
+#define COIN_NULL 0
 
-#define MOTOR_OUT 12
-#define MOTOR_IN 13
+#define MOTOR_OUT 1
+#define MOTOR_IN 0
 
+#define ledPin 13
 
 #define UNDETERMINED_PHASE -1
 
@@ -38,8 +39,6 @@
 #define PRETEST_FAILED 2
 #define PRETEST_LISTEN 3
 
-
-
 SoftwareSerial coinSerial = SoftwareSerial(COIN_IN, COIN_NULL);
 SoftwareSerial motorSerial = SoftwareSerial(MOTOR_IN,MOTOR_OUT);
 
@@ -57,14 +56,16 @@ int escape = 0;
 void setup() {
   // initialize serial communication at 9600 bits per second:
 
-  pinMode(COIN_IN, INPUT);
-  pinMode(MOTOR_IN, INPUT);
-  pinMode(MOTOR_OUT, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 
-  Serial.begin(9600);
-  Serial.println("fish thing ready ++");
+  pinMode(COIN_IN, INPUT);
+//  pinMode(MOTOR_IN, INPUT);
+//  pinMode(MOTOR_OUT, OUTPUT);
+
+  Serial.begin(4800);
+  //Serial.println("fish thing ready ++");
   coinSerial.begin(4800);
-  motorSerial.begin(4800);
+  //motorSerial.begin(4800);
 
   //setCoinPhase();
 }
@@ -75,13 +76,15 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
 
-  testControllersConnection();
+  //testControllersConnection();
+
+  testState = PRETEST_PASSED;
 
   if ( testState == PRETEST_PASSED ) {
     sendData = NO_DATA;
     //readButtons();
-    readCoinMachine();
-    readMotorController();
+    readCoinMachineDIRECT();
+    readMotorControllerDIRECT();
   }
 }
 
@@ -100,11 +103,23 @@ void readCoinMachine(){
   }
 }
 
+void readCoinMachineDIRECT(){
+  if (coinSerial.available()) {
+    int c=coinSerial.read();
+    if ( c != EOL ) {
+      sendData = c;
+      //debugChar(c);
+      fastBlink();
+      //setMotorPhase();
+    }
+  }
+}
 
 void readMotorController(){
   if ( motorPhase() ) {
     if ( sendData != NO_DATA ) {
       motorSerial.print(sendData);
+      sendData = NO_DATA;
     }
 
     if (motorSerial.available()) {
@@ -116,6 +131,24 @@ void readMotorController(){
     }
   }
 }
+
+void tryWriteDIRECT(){
+  if ( sendData != NO_DATA ) {
+    Serial.print(sendData);
+  }
+}
+
+void readMotorControllerDIRECT(){
+  if (Serial.available()) {
+    int m = Serial.read();
+    if ( m != EOL ) {
+      slowBlink();
+      //debugChar(m);
+      //setCoinPhase();
+    }
+  }
+}
+
 
 void readButtons(){
 
@@ -144,6 +177,24 @@ bool motorPhase(){
   return ( commPhase == MOTOR_PHASE );
 }
 
+void fastBlink(){
+  blink( 9, 300 );
+  tryWriteDIRECT();
+}
+
+void slowBlink(){
+  blink(3, 900);
+}
+
+void blink( int count, int delayTime ){
+  for ( int i=0; i<count; i++) {
+    digitalWrite(ledPin, HIGH);   // sets the LED on
+    delay(delayTime);                  // waits for a third second
+    digitalWrite(ledPin, LOW);    // sets the LED off
+    delay(delayTime);
+  }
+}
+
 bool testControllersConnection(){
     if ( testState == PRETEST_INIT ) {
       Serial.println("Testing Connection SEND");
@@ -159,11 +210,15 @@ bool testControllersConnection(){
         int m = motorSerial.read();
         debugChar(m);
         if ( m != EOL && m == HANDSHAKE ) {
+          debugChar('7');
           debugChar(m);
           return true;
+          testState = PRETEST_PASSED;
         }
+      } else {
+        testState = PRETEST_INIT;
       }
       escape++;
-      if ( escape > 10000 ) { testState = PRETEST_FAILED;}
+      if ( escape > 10000 ) { testState = PRETEST_FAILED; }
     }
 }
